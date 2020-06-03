ec9f32bb869fdecc817a62201cdea6c0d9eeba21
liujianxu <1473990960>
2020-06-03 23:14:07 +0800

    配置ssm

---
+++ src/main/java/com/gx/wuzhou/medical/mapper/SysUserMapper.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (C), 2019-2020, XXX有限公司
+ * FileName: SysUserMapper
+ * Author:   Administrator
+ * Date:     2020/6/3 0003 23:04
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.gx.wuzhou.medical.mapper;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author Administrator
+ * @create 2020/6/3 0003
+ * @since 1.0.0
+ */
+public class SysUserMapper {
+}
